722fc55e8e0dd3ff77768806551b782daa2916d4
badal-1 <81519542+badal-1@users.noreply.github.com>
2022-03-21 06:50:05 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,28 @@
+img ="";
+cocossed_status = ""; 
+function go1(){
+window.location.href="a.html";
+}
+function preload(){
+img = loadImage('img1.png');
+}
+function setup(){
+canvas=createCanvas(640, 420);
+canvas.center();
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("Status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded(){
+console.log("Model Loaded!");
+cocossed_status = true;
+objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results){
+if(error){
+console.log(error);
+}
+console.log(results);
+}
+function draw(){
+image(img, 0, 0, 640, 420);
+}
